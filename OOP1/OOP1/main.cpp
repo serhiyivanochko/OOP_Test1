c4454aa1f31eee64d732597a95fe2b43d6a90fdb
Serhiy Ivanochko <ivanochko@itstep.academy>
2022-08-28 12:59:09 +0300

    Created project

---
+++ OOP1/OOP1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OOP1
+//
+//  Created by Serhiy Ivanochko on 28.08.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
